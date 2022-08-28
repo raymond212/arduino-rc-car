cd0c1596ffe484d543f1758115bcbc5aff4634e3
Raymond <ruiwenhw@gmail.com>
2022-08-27 17:28:35 -0700

    improved bluetooth user interface

--- button-controlled-rc-car/ButtonControlledRcCarApp/app/src/main/java/com/example/buttoncontrolledrccarapp/MainActivity.kt
+++ button-controlled-rc-car/ButtonControlledRcCarApp/app/src/main/java/com/example/buttoncontrolledrccarapp/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.buttoncontrolledrccarapp
 
 import android.annotation.SuppressLint
-import android.app.ProgressDialog
 import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothSocket
-import android.content.Context
 import android.content.Intent
-import android.os.AsyncTask
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import java.io.IOException
 import java.util.*
 
@@ -23,7 +18,6 @@ class MainActivity : AppCompatActivity() {
     companion object {
         var m_myUUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
         var m_bluetoothSocket: BluetoothSocket? = null
-        lateinit var m_progress: ProgressDialog
         lateinit var m_bluetoothAdapter: BluetoothAdapter
         var m_isConnected: Boolean = false
         lateinit var m_address: String
@@ -34,39 +28,51 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         title = "Button Controlled RC Car"
-
-        val connectBtn = findViewById<Button>(R.id.connectBluetoothBtn)
-        val disconnectBtn = findViewById<Button>(R.id.disconnectBluetoothBtn)
-        val forwardBtn = findViewById<ImageButton>(R.id.forwardBtn)
-        val backwardBtn = findViewById<ImageButton>(R.id.backwardBtn)
-        val leftBtn = findViewById<ImageButton>(R.id.leftBtn)
-        val rightBtn = findViewById<ImageButton>(R.id.rightBtn)
-        val stopBtn = findViewById<ImageButton>(R.id.stopBtn)
-
+        val connectBtn: Button = findViewById(R.id.connectBluetoothBtn)
         connectBtn.setOnClickListener {
             val intent = Intent(this, ConnectBluetoothActivity::class.java)
             startActivity(intent)
         }
+        buttonsOff()
+    }
 
-        m_address = intent.getStringExtra(ConnectBluetoothActivity.EXTRA_ADDRESS).toString()
-        val connected: String = intent.getStringExtra(ConnectBluetoothActivity.CONNECTED).toString()
-        if (connected == "YES") {
-            ConnectToDevice(this).execute()
-
-            disconnectBtn.setOnClickListener { disconnect() }
-            forwardBtn.setOnClickListener { sendCommand("1") }
-            backwardBtn.setOnClickListener { sendCommand("2") }
-            leftBtn.setOnClickListener { sendCommand("3") }
-            rightBtn.setOnClickListener { sendCommand("4") }
-            stopBtn.setOnClickListener { sendCommand("0") }
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        if (m_isConnected) {
+            buttonsOn()
         } else {
-            forwardBtn.setOnClickListener { toast("No bluetooth device connected") }
-            backwardBtn.setOnClickListener { toast("No bluetooth device connected") }
-            leftBtn.setOnClickListener { toast("No bluetooth device connected") }
-            rightBtn.setOnClickListener { toast("No bluetooth device connected") }
-            stopBtn.setOnClickListener { toast("No bluetooth device connected") }
+            buttonsOff()
         }
+    }
 
+    private fun buttonsOn() {
+        val disconnectBtn: Button = findViewById(R.id.disconnectBluetoothBtn)
+        val forwardBtn: ImageButton = findViewById(R.id.forwardBtn)
+        val backwardBtn: ImageButton = findViewById(R.id.backwardBtn)
+        val leftBtn: ImageButton = findViewById(R.id.leftBtn)
+        val rightBtn: ImageButton = findViewById(R.id.rightBtn)
+        val stopBtn: ImageButton = findViewById(R.id.stopBtn)
+        disconnectBtn.setOnClickListener { disconnect() }
+        forwardBtn.setOnClickListener { sendCommand("1") }
+        backwardBtn.setOnClickListener { sendCommand("2") }
+        leftBtn.setOnClickListener { sendCommand("3") }
+        rightBtn.setOnClickListener { sendCommand("4") }
+        stopBtn.setOnClickListener { sendCommand("0") }
+    }
+
+    private fun buttonsOff() {
+        val disconnectBtn: Button = findViewById(R.id.disconnectBluetoothBtn)
+        val forwardBtn: ImageButton = findViewById(R.id.forwardBtn)
+        val backwardBtn: ImageButton = findViewById(R.id.backwardBtn)
+        val leftBtn: ImageButton = findViewById(R.id.leftBtn)
+        val rightBtn: ImageButton = findViewById(R.id.rightBtn)
+        val stopBtn: ImageButton = findViewById(R.id.stopBtn)
+        disconnectBtn.setOnClickListener { toast("No bluetooth device connected") }
+        forwardBtn.setOnClickListener { toast("No bluetooth device connected") }
+        backwardBtn.setOnClickListener { toast("No bluetooth device connected") }
+        leftBtn.setOnClickListener { toast("No bluetooth device connected") }
+        rightBtn.setOnClickListener { toast("No bluetooth device connected") }
+        stopBtn.setOnClickListener { toast("No bluetooth device connected") }
     }
 
     private fun toast(input: String) {
@@ -93,46 +99,6 @@ class MainActivity : AppCompatActivity() {
                 e.printStackTrace()
             }
         }
-    }
-
-    private class ConnectToDevice(c: Context) : AsyncTask<Void, Void, String>() {
-        private var connectSuccess: Boolean = true
-        private val context: Context
-
-        init {
-            this.context = c
-        }
-
-        override fun onPreExecute() {
-            super.onPreExecute()
-            m_progress = ProgressDialog.show(context, "Connecting...", "Please wait")
-        }
-
-        override fun doInBackground(vararg p0: Void?): String? {
-            try {
-                if (m_bluetoothSocket == null || !m_isConnected) {
-                    m_bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
-                    val device: BluetoothDevice = m_bluetoothAdapter.getRemoteDevice(m_address)
-                    m_bluetoothSocket = device.createInsecureRfcommSocketToServiceRecord(m_myUUID)
-                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
-                    m_bluetoothSocket!!.connect()
-                }
-            } catch (e: IOException) {
-                connectSuccess = false
-                e.printStackTrace()
-            }
-            return null
-        }
-
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
-            if (!connectSuccess) {
-                Toast.makeText(this.context, "Could not connect", Toast.LENGTH_SHORT).show()
-            } else {
-                m_isConnected = true
-                Toast.makeText(this.context, "Connected", Toast.LENGTH_SHORT).show()
-            }
-            m_progress.dismiss()
-        }
+        buttonsOff()
     }
 }
